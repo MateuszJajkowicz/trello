b9b0fb8dca8970c9a8fac08ccc19b105ce7703a2
Mateusz Jajkowicz <mateusz.jajkowicz@gmail.com>
2022-05-19 01:10:15 +0200

    1

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    projectId: 'trello-angular-43029',
+    appId: '1:157631531013:web:8c35c581bb03118758346c',
+    databaseURL: 'https://trello-angular-43029-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'trello-angular-43029.appspot.com',
+    apiKey: 'AIzaSyD0gYe9g0myfKnUGELdDxsL_gtN2H_U-cs',
+    authDomain: 'trello-angular-43029.firebaseapp.com',
+    messagingSenderId: '157631531013',
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'trello-angular-43029',
+    appId: '1:157631531013:web:8c35c581bb03118758346c',
+    databaseURL: 'https://trello-angular-43029-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'trello-angular-43029.appspot.com',
+    apiKey: 'AIzaSyD0gYe9g0myfKnUGELdDxsL_gtN2H_U-cs',
+    authDomain: 'trello-angular-43029.firebaseapp.com',
+    messagingSenderId: '157631531013',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
